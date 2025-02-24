1fa0af7b727e21fab493bc82084c25c3099fa2b0
lintang <lintangprayogo12@gmail.com>
2025-02-24 15:35:09 +0700

    -

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -7,4 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 class Attendance extends Model
 {
     //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'date',
+        'time_in',
+        'time_out',
+        'latlon_in',
+        'latlon_out',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
